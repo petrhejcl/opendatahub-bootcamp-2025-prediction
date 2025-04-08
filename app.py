65cfb24e30ce31b96c2453c10b9c22de68a75c48
Giacomo Pilotti <jack.pilo04@gmail.com>
2025-04-08 13:49:47 +0200

    Implement simplest UI

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from datetime import date
+
+from get_data import get_data
+
+
+st.set_page_config(layout="wide")
+
+# Streamlit UI
+st.title("Station Data Fetcher")
+
+# User inputs
+station_code = st.text_input("Enter Station Code")
+start_date = str(st.date_input("Start Date", value=date.today()))
+end_date = str(st.date_input("End Date", value=date.today()))
+
+# Ensure start date is before end date
+if start_date > end_date:
+    st.error("Start date must be before or equal to end date.")
+else:
+    # Button to trigger the function
+    if st.button("Fetch Data", use_container_width=True, type="primary"):
+        df = get_data(
+            station_code=station_code, start_date=start_date, end_date=end_date
+        )
+        st.success("Data retrieved successfully!")
+
+        st.dataframe(df)
---
+++ get_data.py
@@ -0,0 +1,74 @@
+import requests
+import json
+import csv
+import pandas as pd
+
+
+def get_data(station_code: str, start_date: str, end_date: str) -> pd.DataFrame:
+    url = f"https://mobility.api.opendatahub.com/v2/flat/ParkingStation/free,occupied/{start_date}/{end_date}?where=and%28sorigin.eq.FAMAS%2Cscode.eq.%22{station_code}%22%29&select=mvalue,mvalidtime,sname,scode,sorigin,tname&limit=-1"
+
+    # TODO: Maybe add this to separate file
+    token_url = (
+        "https://auth.opendatahub.com/auth/realms/noi/protocol/openid-connect/token"
+    )
+
+    token_headers = {"Content-Type": "application/x-www-form-urlencoded"}
+
+    token_body = {
+        "grant_type": "client_credentials",
+        # TODO: Add these to env secrets
+        "client_id": "opendatahub-bootcamp-2025",
+        "client_secret": "QiMsLjDpLi5ffjKRkI7eRgwOwNXoU9l1",
+    }
+
+    token_response = requests.post(token_url, headers=token_headers, data=token_body)
+
+    bearer_token = token_response.json().get("access_token")
+
+    # Set headers
+    headers = {"Authorization": f"Bearer {bearer_token}", "Accept": "application/json"}
+
+    # Perform the GET request
+    try:
+        response = requests.get(url, headers=headers)
+        print(response.headers)
+        response.raise_for_status()  # Raise an error for bad status codes\
+        print(response.status_code)
+        data = response.json().get("data")  # Parse JSON response
+
+        with open("parking.csv", "w", newline="") as csvfile:
+            parking_writer = csv.writer(
+                csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL
+            )
+            parking_writer.writerow(["mvalidtime", "free", "occupied"])
+
+            data_dict = dict()
+
+            for measurement in data:
+                if measurement["mvalidtime"] in data_dict:
+                    data_dict[measurement["mvalidtime"]].update(
+                        {measurement["tname"]: measurement["mvalue"]}
+                    )
+                else:
+                    data_dict[measurement["mvalidtime"]] = {
+                        measurement["tname"]: measurement["mvalue"]
+                    }
+
+            for mvalidtime, data in data_dict.items():
+                parking_writer.writerow(
+                    [mvalidtime, data.get("free"), data.get("occupied")]
+                )
+
+            print(f"Response saved to {csvfile}")
+
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+    except json.JSONDecodeError:
+        print("Response was not valid JSON.")
+
+    return pd.read_csv("parking.csv")
+
+
+if __name__ == "__main__":
+    df = get_data(station_code="116", start_date="2025-04-07", end_date="2025-04-08")
+    print(df)
